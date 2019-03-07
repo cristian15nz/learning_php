ee55a781a7da586769bf18d6e500f13d81fbe951
Cristian <cristian15nz@gmail.com>
2019-03-07 12:04:13 -0400

    Comando echo

---
+++ practicas/1_primer_script_php/comando_echo.php
@@ -0,0 +1,22 @@
+<?php
+
+$nombre = "Cristian"; // string
+$edad = 23; // int
+$tieneHijos = false; // boolean
+
+echo $nombre;
+echo "<br>";
+echo $edad;
+echo "<br>";
+echo $tieneHijos;
+
+
+$nombre = true;
+$edad = "Hola";
+$tieneHijos = 2222;
+
+echo $nombre;
+echo "<br>";
+echo $edad;
+echo "<br>";
+echo $tieneHijos;
