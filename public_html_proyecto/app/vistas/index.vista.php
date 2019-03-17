89abf4d9de7e01f41e62e2ec5e330a88a7b09232
Cristian <cristian15nz@gmail.com>
2019-03-17 09:57:11 -0400

    Fix picture on responsive

--- public_html_proyecto/app/vistas/index.vista.php
+++ public_html_proyecto/app/vistas/index.vista.php
@@ -1,16 +1,21 @@
 <?php require '_head.php'; ?>
 
 <body class="bg-light">
-    <div class="container fondo-laptop">
-        <section id="bienvenido">
-            <h1>Bienvenidos a <span class="d-inline-block text-xtudio"><i class="fab fa-xing-square"></i> XtudioPlay</span></h1>
-            <p class="lead">Un lugar donde puedes compartir tus ideas.</p>
-            <br>
-            <div>
-                <a class="btn btn-primary" href="login.php">Iniciar sesión</a>
-                <span class="m-1">o</span>
-                <a class="text-link" href="registro.php">Registrarse</a>
-            </div>
+    <div class="container">
+        <section class="row text-center vh-100 align-items-center justify-content-center">
+            <aside class="col-md-6">
+                <h1>Bienvenidos a <span class="d-inline-block text-xtudio"><i class="fab fa-xing-square"></i> XtudioPlay</span></h1>
+                <p class="lead">Un lugar donde puedes compartir tus ideas.</p>
+                <br>
+                <div>
+                    <a class="btn btn-primary" href="login.php">Iniciar sesión</a>
+                    <span class="m-1">o</span>
+                    <a class="text-link" href="registro.php">Registrarse</a>
+                </div>
+            </aside>
+            <aside class="col-md-6">
+                <img src="recursos/img/laptop.svg" style="width: 90%;" alt="">
+            </aside>
         </section>
     </div>
 </body>
